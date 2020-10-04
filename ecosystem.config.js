e226a8fe6bdbcd7bab35aa9e9724aeb83137094d
Glenn Hall <glennerichall@hotmail.com>
2020-10-04 10:28:57 -0400

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    apps: [
+        {
+            script: 'index.js',
+            watch: 'true',
+            name: 'motion-ui',
+            env: {
+                env_production: {
+                    NODE_ENV: 'production',
+                    API_HOST: 'http://192.168.1.110:3000',
+                    STREAM_HOST: 'https://zm.velor.ca'
+                }
+            }
+        }
+    ],
+
+    deploy: {
+        production: {
+            user: 'glenn',
+            host: '192.168.1.110',
+            ref: 'origin/master',
+            repo: 'git@github.com:glennerichall/motion-ui.git',
+            path: '~/motion-ui',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+        }
+    }
+};
