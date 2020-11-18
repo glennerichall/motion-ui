de46a31cc888e03a79633a86b1794ac7d3d548df
Glenn Hall <glennerichall@hotmail.com>
2020-11-17 21:52:03 -0500

    dang

--- views/js/socket.js
+++ views/js/socket.js
@@ -3,5 +3,6 @@ import {io} from 'socket.io-client';
 const protocol = location.protocol === 'https' ? 'wss' : 'ws';
 
 export const socket = io(`${protocol}://${location.host}/`, {
-    reconnectionDelayMax: 10000
+    reconnectionDelayMax: 10000,
+    secure: protocol === 'wss'
 });
