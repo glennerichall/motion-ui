3a77ad82bcaede89c70ee8a6a6b7979b554c5169
Glenn Hall <glennerichall@hotmail.com>
2020-11-17 21:54:45 -0500

    kalis

--- views/js/socket.js
+++ views/js/socket.js
@@ -2,6 +2,8 @@ import {io} from 'socket.io-client';
 
 const protocol = location.protocol === 'https' ? 'wss' : 'ws';
 
+console.log(`${protocol}://${location.host}/`);
+
 export const socket = io(`${protocol}://${location.host}/`, {
     reconnectionDelayMax: 10000,
     secure: protocol === 'wss'
