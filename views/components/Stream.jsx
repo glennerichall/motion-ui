0774c8432472f9f2d7abf9e23f7e10274d4e4237
Glenn Hall <glennerichall@hotmail.com>
2023-04-23 12:03:41 -0400

    deploying

--- views/components/Stream.jsx
+++ views/components/Stream.jsx
@@ -80,7 +80,9 @@ export default props => {
                     <StreamInfo stream={props.stream}>{name}</StreamInfo>
                 </div>
 
-                {connectionStatus === 'idle' || connectionStatus === 'recording' ?
+                {connectionStatus === 'idle' ||
+                connectionStatus === 'recording' ||
+                connectionStatus === 'connection-ok' ?
                     <img src={url} draggable="false"
                          onError={(e) => console.log(`camera image error ${id}`)}
                          onStalled={() => console.log('stalled')}
